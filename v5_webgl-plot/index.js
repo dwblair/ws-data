23ae591c366c17e6fa99eb438395300dcc134054
cversek <cversek@gmail.com>
2021-07-03 23:31:06 -0400

    allow external style.css to be served

--- v5_webgl-plot/index.js
+++ v5_webgl-plot/index.js
@@ -10,9 +10,15 @@ const parser = serport.pipe(new Readline({ delimiter: '\r\n' }))
 //parser.on('data', console.log)
 
 parser.on('data', function (data) {
-  console.log(data);
+  //console.log(data);
   io.emit('adc', data.toString());
 })
+
+app.get('/style.css', (req, res) => {
+  console.log('style.css requested!')
+  res.type('.css');
+  res.sendFile(__dirname + '/style.css');
+});
 
 app.get('/wsplot.js', (req, res) => {
   console.log('wsplot.js requested!')
